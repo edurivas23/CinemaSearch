f157045d35af6a19415895c117646498d4f081a7
edurivas23 <edu.samayoa1@gmail.com>
2026-08-21 20:09:39 -0600

    fix navigate from hero

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,22 +1,20 @@
-
 export interface Movie {
-    id: number;
-    title: string;
-    genre_ids:number[];
-    release_date: string;
-    vote_average: number;
-    poster_path: string;
-    backdrop_path: string;
-    overview: string;
+  id: number;
+  title: string;
+  genre_ids: number[];
+  release_date: string;
+  vote_average: number;
+  poster_path: string;
+  backdrop_path: string;
+  overview: string;
 }
 
 export interface useMoviesProps {
-    category?: string;
-    title?: string
-
+  category?: string;
+  title?: string;
 }
 
 export interface Genre {
-    id:number;
-    name:string;
+  id: number;
+  name: string;
 }
--- src/pages/AboutMovie.tsx
+++ src/pages/AboutMovie.tsx
@@ -1,38 +1,35 @@
-import { Navigate, useLocation } from "react-router"
-import type { Movie, Genre } from "../interfaces"
-
+import { Navigate, useLocation } from "react-router";
+import type { Movie, Genre } from "../interfaces";
 
 export const AboutMovie = () => {
+  const { state } = useLocation();
+  const movie = state?.movie as Movie;
+  const movieGenres = state?.movieGenres as Genre[];
 
-  const { state } = useLocation()
-  const movie = state?.movie as Movie
-  const movieGenres = state?.movieGenres as Genre[]
-
-
-
-
-
-  if (!movie) return <Navigate to="/404" replace />
+  if (!movie) return <Navigate to="/404" replace />;
 
   return (
-    <main className=" relative z-10  max-w-7xl mx-auto px-5" >
-
+    <main className=" relative z-10  max-w-7xl mx-auto px-5">
       <aside className="h-full flex flex-col w-60 gap-4">
-        <div >
-          <img className="rounded-md ring-2 ring-white" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} width={240} />
+        <div>
+          <img
+            className="rounded-md ring-2 ring-white"
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+            alt={movie.title}
+            width={240}
+          />
         </div>
-        <div className="grid grid-cols-3 gap-2">
-          {movieGenres?.map(genre => (
-            <span className="bg-gray-700 py-2 shadow-lg cursor-default text-white gap-2 font-bold rounded-sm text-xs text-center hover:brightness-120 transition-all " key={genre.id}>{genre.name}</span>
+        <div className="flex flex-wrap gap-2">
+          {movieGenres?.map((genre) => (
+            <span
+              className="bg-gray-700 p-2 shadow-lg cursor-default text-white gap-2 font-bold rounded-sm text-xs text-center hover:brightness-120 transition-all "
+              key={genre.id}
+            >
+              {genre.name}
+            </span>
           ))}
         </div>
       </aside>
-
-
-
-
-
-
     </main>
-  )
-}
+  );
+};
